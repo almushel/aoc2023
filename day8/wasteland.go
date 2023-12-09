b500c9f5c2c6a5234833c48e11ae2869756990fb
Andrew Mushel <almushel@protonmail.com>
2023-12-09 13:06:58 -0600

    Day 8: Haunted Wasteland

---
+++ day8/wasteland.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"slices"
+	"strings"
+)
+
+func ParseInput(input string) (directions string, network map[string][2]string) {
+	network = make(map[string][2]string)
+	directions, input, found := strings.Cut(input, "\n\n")
+	if !found {
+		return
+	}
+	for _, line := range strings.Split(input, "\n") {
+		node := line[:3]
+		edges := strings.Split(line[len("XXX = ("):len(line)-1], ", ")
+		network[node] = [2]string{edges[0], edges[1]}
+	}
+	return
+}
+
+func SolvePart1(input string) (result int) {
+	directions, network := ParseInput(input)
+
+	found := false
+	currentNode := "AAA"
+	for !found {
+		for _, dir := range directions {
+			result++
+			if dir == 'L' {
+				currentNode = network[currentNode][0]
+			} else {
+				currentNode = network[currentNode][1]
+			}
+			if currentNode == "ZZZ" {
+				found = true
+			}
+		}
+	}
+
+	return
+}
+
+func getPrimeFactors(num int) []int {
+	var result []int
+
+	for num%2 == 0 {
+		result = append(result, 2)
+		num /= 2
+	}
+
+	for i := 3; i < num; i += 2 {
+		for num%i == 0 {
+			result = append(result, i)
+			num /= i
+		}
+	}
+
+	if num > 2 {
+		result = append(result, num)
+	}
+
+	return result
+}
+
+func getUniqueFactors(nums []int) (commonFactors []int) {
+	factors := make([][]int, len(nums))
+	maxFactors := 0
+	for i, _ := range factors {
+		factors[i] = getPrimeFactors(nums[i])
+		length := len(factors[i])
+		if length > maxFactors {
+			maxFactors = length
+		}
+	}
+
+	for i := 0; i < maxFactors; i++ {
+		var found []int
+		for f, _ := range factors {
+			if i >= len(factors[f]) {
+				continue
+			}
+			checkFactor := factors[f][i]
+			if !slices.Contains(found, checkFactor) {
+				found = append(found, checkFactor)
+			}
+		}
+		commonFactors = append(commonFactors, found...)
+	}
+	return
+}
+
+func SolvePart2(input string) (result int) {
+	directions, network := ParseInput(input)
+
+	var entrances []string
+	for node, _ := range network {
+		if node[2] == 'A' {
+			entrances = append(entrances, node)
+		}
+	}
+	var paths []int
+	for _, entrance := range entrances {
+		currentNode := entrance
+		count := 0
+		found := false
+		for {
+			for _, dir := range directions {
+				count++
+				if dir == 'L' {
+					currentNode = network[currentNode][0]
+				} else {
+					currentNode = network[currentNode][1]
+				}
+
+				if currentNode[2] == 'Z' {
+					paths = append(paths, count)
+					found = true
+					break
+				}
+			}
+			if found {
+				break
+			}
+		}
+	}
+	result = 1
+	commonFactors := getUniqueFactors(paths)
+	for _, factor := range commonFactors {
+		result *= factor
+	}
+	return
+}
+
+func main() {
+	buffer, err := os.ReadFile("data/day8_input.txt")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	str := strings.TrimSpace(string(buffer))
+
+	part1Result := SolvePart1(str)
+	part2Result := SolvePart2(str)
+
+	fmt.Println(part1Result)
+	fmt.Println(part2Result)
+	return
+}
---
+++ day8/wasteland_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/almushel/aoc2023/utility"
+)
+
+const (
+	exampleSolution1 = 2
+	exampleSolution2 = 6
+	part1Solution    = 19951
+	part2Solution    = 16342438708751
+)
+
+var testInput1 string = `RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)`
+
+var testInput2 string = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`
+
+var input string
+
+func init() {
+	buffer, err := os.ReadFile("../data/day8_input.txt")
+	if err != nil {
+		println(err.Error)
+		os.Exit(1)
+	}
+	input = strings.TrimSpace(string(buffer))
+}
+func TestPart1Example(t *testing.T) {
+	utility.PuzzleTest(t, testInput1, exampleSolution1, SolvePart1)
+}
+
+func TestPart2Example(t *testing.T) {
+	utility.PuzzleTest(t, testInput2, exampleSolution2, SolvePart2)
+}
+
+func TestPart1(t *testing.T) {
+	utility.PuzzleTest(t, input, part1Solution, SolvePart1)
+}
+
+func TestPart2(t *testing.T) {
+	utility.PuzzleTest(t, input, part2Solution, SolvePart2)
+}
